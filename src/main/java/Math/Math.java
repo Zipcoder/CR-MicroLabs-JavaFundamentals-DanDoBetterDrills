c69cae7f948f2151c3b5820ac5f8662e628a5d92
rob <rob@zipcoders-MacBook-Pro.local>
2017-10-28 22:08:19 -0400

    finished lab

--- src/main/java/Math/Math.java
+++ src/main/java/Math/Math.java
@@ -7,57 +7,117 @@ public class Math {
 
     //Get the remainder from division
     public int remainder(int one, int two){
-        return 0;
+        return one % two;
     }
 
     //Return true using greater than symbol
     public boolean greaterThanTrue(int one, int two){
-        return false;
+        boolean answer;
+        if (one > two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return false using greater than symbol
     public boolean greaterThanFalse(int one, int two){
-        return true;
+        boolean answer;
+        if (one > two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return true using less than symbol
     public boolean lessThanTrue(int one, int two){
-        return false;
+        boolean answer;
+        if (one < two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return false using less than symbol
     public boolean lessThanFalse(int one, int two){
-        return true;
+        boolean answer;
+        if (one < two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return true using greater or equal symbol
     public boolean greaterOrEqualTrue(int one, int two){
-        return false;
+        boolean answer;
+        if (one >= two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return false using greater or equal symbol
     public boolean greaterOrEqualFalse(int one, int two){
-        return true;
+        boolean answer;
+        if (one >= two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return true using less than or equal symbol
     public boolean lessOrEqualTrue(int one, int two){
-        return false;
+        boolean answer;
+        if (one <= two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return false using less than or equal symbol
     public boolean lessOrEqualFalse(int one, int two){
-        return true;
+        boolean answer;
+        if (one <= two){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return true if the number in parameter is equal to 3 and less than 90
     public boolean logicalAnd(Integer number){
-        return false;
+        boolean answer;
+        if (number == 3 && number < 90){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
     //Return true if the number passed in is equal to 4 or greater than 100
     public boolean logicalOr(Integer number){
-        return false;
+        boolean answer;
+        if (number == 4 || number > 100){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
     }
 
 }
--- src/main/java/Strings/Strings.java
+++ src/main/java/Strings/Strings.java
@@ -7,52 +7,68 @@ public class Strings {
 
     //Concatenate two strings together that are passed in the parameters
     public String concatenation(String one, String two){
-        return null;
+        String output = one + two;
+        return output;
     }
 
     //Concatenate a string and a integer together that are passed in the parameters
     public String concatenation(int one, String two){
-        return null;
+        String output = one + two;
+        return output;
     }
 
     //Get the substring of the first three letters of a string
     public String subStringBegin(String input){
-        return null;
+        String output = input.substring(0, 3);
+        return output;
     }
 
     //Get the substring of a string "Hello" so it returns the last three letters only
     public String subStringEnd(String input){
-        return null;
+        String output = input.substring(input.length() - 3);
+        return output;
     }
 
     //Compare the two strings using compareTo() and if they are return true, else false
     public boolean compareTwoStrings(String one, String two){
-        return false;
+        int answer = one.compareTo(two);
+        if (answer == 0){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //Compare the two strings using equals() and if they are return true, else false
     public boolean compareTwoStringsEqual(String one, String two){
-        return false;
+        if (one.equals(two)){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     //Write a method that returns the middle character in the given string hint: use the .length and .charAt methods
     public char getTheMiddleChar(String string){
-        return ' ';
+        int indexToRemove = (string.length() / 2);
+        return string.charAt(indexToRemove);
     }
 
     //Use the indexOf method to find the first space in a string and .substring() to return the first word
     public String getTheFirstWord(String string){
-        return null;
+        return string.substring(0, string.indexOf(' '));
     }
 
     //Use the same behavior to find the second word
     public String getTheSecondWord(String string){
-        return null;
+        String subString = string.substring(string.indexOf(' ') + 1);
+        return subString;
     }
 
     //Create a method that uses the above methods to return a string consisting of the second and first word in reversed order
     public String reverseTheTwo(String string){
-        return null;
+        String substring = string.substring(string.indexOf(' ') + 1) + " " + string.substring(0, string.indexOf(' '));
+        return substring;
     }
 
 }

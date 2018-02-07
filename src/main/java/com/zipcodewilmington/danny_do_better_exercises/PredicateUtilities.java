ecc5bc15b9a4ad4929758ec13a5385eb9a216ec1
Sean Mis <seanmis@MacBook-Pro-3.local>
2018-02-07 16:30:12 -0500

    Danny Do edit one

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -9,8 +9,10 @@ public class PredicateUtilities {
      * @param y
      * @return true if `x` is greater than `y`
      */
+
+
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        return x>y;
     }
 
     /**
@@ -19,7 +21,7 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        return x<y;
     }
 
     /**
@@ -28,7 +30,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        return x>=y;
     }
 
     /**
@@ -37,6 +39,6 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return x<=y;
     }
 }

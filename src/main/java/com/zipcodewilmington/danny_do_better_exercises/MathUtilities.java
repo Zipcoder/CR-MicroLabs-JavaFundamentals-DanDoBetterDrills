18bc2d26d05678668ee4a02cb5a70618e06402a5
Owen Murphy <owenmurphy@zipcoders-MacBook-Pro-3.local>
2018-02-07 15:21:46 -0500

    doesn't matter

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -29,6 +29,7 @@ public class MathUtilities {
      * @return sum of `baseValue` and `difference`
      */
     public Short add(short baseValue, short difference) {
+
         return null;
     }
 

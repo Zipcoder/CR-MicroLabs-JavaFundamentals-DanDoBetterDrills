0b792a20bb19eecf8c27db64842095ba7378f035
Jordan Elderidge <jordanelderidge@zipcoders-MacBook-Pro.local>
2018-02-07 09:37:36 -0500

    Mostly done just have to finish Test String Utilities and do the Math Utilities

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,8 +10,9 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        return (x > y);
     }
+
 
     /**
      * @param x
@@ -19,7 +20,7 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        return (x < y);
     }
 
     /**
@@ -28,7 +29,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        return (x >= y);
     }
 
     /**
@@ -37,6 +38,6 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return (x <= y);
     }
 }

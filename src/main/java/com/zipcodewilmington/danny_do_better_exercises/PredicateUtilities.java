bdd73ec104eed3792d61d14968a3a3d13b4b87c4
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-07 08:38:36 -0500

    here is my math

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,8 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+
+        return x > y ? true:false;
     }
 
     /**
@@ -19,7 +20,7 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        return x < y ? true:false;
     }
 
     /**
@@ -28,7 +29,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        return x>=y ? true:false;
     }
 
     /**
@@ -37,6 +38,6 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return x<=y? true:false;
     }
 }

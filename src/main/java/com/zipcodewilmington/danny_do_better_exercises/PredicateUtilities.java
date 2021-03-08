81718b519568c459308019ca6ce9d11a3dbe22fd
smartist17 <79124739+smartist17@users.noreply.github.com>
2021-03-07 19:33:46 -0500

    Work in progress

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,49 +10,65 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        if (x > y) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    /**
-     * @param x
-     * @param y
-     * @return true if `x` is less than `y`
-     */
-    public Boolean isLessThan(int x, int y) {
-        return null;
+        /**
+         * @param x
+         * @param y
+         * @return true if `x` is less than `y`
+         */
+        public Boolean isLessThan ( int x, int y){
+            if (x < y) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * @param x
+         * @param y
+         * @return true if `x` is greater than or equal to `y`
+         */
+        public Boolean isGreaterThanOrEqualTo ( int x, int y){
+            if (x >= y) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * @param x
+         * @param y
+         * @return true if `x` is less than or equal to `y`
+         */
+        public Boolean isLessThanOrEqualTo ( int x, int y){
+            if (x <= y) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+
+        /**
+         * @return true
+         */
+        public Boolean returnTrue () {
+            return true;
+        }
+
+        /**
+         * @return false
+         */
+        public Boolean returnFalse () {
+            return false;
+        }
+
     }
-
-    /**
-     * @param x
-     * @param y
-     * @return true if `x` is greater than or equal to `y`
-     */
-    public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
-    }
-
-    /**
-     * @param x
-     * @param y
-     * @return true if `x` is less than or equal to `y`
-     */
-    public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
-    }
-
-
-    /**
-     * @return true
-     */
-    public Boolean returnTrue() {
-        return null;
-    }
-
-    /**
-     * @return false
-     */
-    public Boolean returnFalse() {
-        return null;
-    }
-
-}
